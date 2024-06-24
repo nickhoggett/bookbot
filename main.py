1a83797cdc18e4bfbefb0460746018a327691d79
nick.hoggett@gmail.com <nick.hoggett@gmail.com>
2024-06-24 09:29:03 +0100

    added python reading file and counting words in book

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
-    with open("books/Frankenstein.txt") as f:
+    book_path = "books/Frankenstein.txt"
+    book_text = read_file(book_path)
+    words = word_count(book_text)
+    print(words)
+
+def read_file(file):
+    with open(file) as f:
         file_contents = f.read()
-        return print(file_contents)
+        return file_contents
+
+def word_count(book):
+    words = book.split()
+    return (len(words))
 
 main()
